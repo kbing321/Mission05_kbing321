7e67d8d68d3705a35f9ea1626f0de0f960750440
Kohlton Bingham <kohltonhbingham@gmail.com>
2023-02-06 18:47:22 -0700

    Started transition to MVC

---
+++ Mission04_kbing321/Controllers/BlahController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_kbing321.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+    }
+}
